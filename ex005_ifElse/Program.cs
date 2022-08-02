d3ba1a012080c9705b02647ca37b7fa2572a5a55
Vyacheslav <vyacheslav.trubaev@mail.ru>
2022-08-02 19:55:44 +0300

    add ex005

---
+++ ex005_ifElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Ввведите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура это же маша");
+    
+}
+else
+{
+    Console.Write("привет, ");
+    Console.WriteLine(username);
+}
